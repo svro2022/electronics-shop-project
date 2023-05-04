10e1622ffc8e20558991af806f5508341f5a383c
svro <ichistrawberry2012@gmail.com>
2023-05-04 19:39:10 +0300

    change test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def test_item():
+    return Item("headphones", 2300, 5)
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 11500
+
+def test_apply_discount(test_item):
+    Item.pay_rate = 0.5
+    test_item.apply_discount()
+    assert test_item.price == 1150
+
+def test_name_getter(test_item):
+    assert test_item.name == "headphones"
