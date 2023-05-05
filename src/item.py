8a87d43351e6e7696de7172788191c86039afbe4
svro <ichistrawberry2012@gmail.com>
2023-05-05 15:23:39 +0300

    change test_item.py again

--- src/item.py
+++ src/item.py
@@ -31,4 +31,4 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * self.pay_rate
+        self.price *= self.pay_rate
