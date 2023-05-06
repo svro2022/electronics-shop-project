ebb69cb732d3ed7b2c8709dc087eade9fa727b50
svro <ichistrawberry2012@gmail.com>
2023-05-06 18:18:57 +0300

    change item.py for 2h

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +15,26 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        super().__init__()
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
+
+    """Декоратор @property (геттер) делает атрибут __name доступным извне"""
+    @property
+    def name(self):
+        return self.__name
+
+    """В сеттере @name.setter проверяем, что длина наименования товара не больше 10 символов"""
+    @name.setter
+    def name(self, value):
+        self.__name = value
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            print("Ошибка! Длина наименования товара больше 10 символов")
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +49,24 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path='../src/items.csv'):
+        """
+        Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv
+        """
+        cls.all.clear()
+        with open(file_path, 'r', encoding='windows-1251') as file:
+            file_reader = csv.DictReader(file, delimiter=',')
+
+            for value in file_reader:
+                name, price, quantity = value.values()
+                cls(name, cls.string_to_number(price), cls.string_to_number(quantity))
+
+    @staticmethod
+    def string_to_number(number):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        return int(float(number))
+
